4dde036cfd3e6530f8dd1990577089b9ddbbe307
Tuss4 <tuss4dbfn@gmail.com>
2016-02-05 12:55:29 -0600

    Should be good.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,3 +3,11 @@ INSTALLED_APPS = [
     'tests',
     'fbauth'
 ]
+
+FACEBOOK_SCOPE = ['email']
+FACEBOOK_PROFILE_FIELDS = ['email', 'first_name', 'last_name']
+FACEBOOK_CLIENT_ID = "LegitClientId"
+FACEBOOK_CLIENT_SECRET = "LegitClientSecret"
+FACEBOOK_EXHANGE_URL = "graph.facebook.com/v2.5/oauth/access_token"
+CALLBACK_HOST = "https://example.com"
+CALLBACK_ENDPOINT = "/fb/callback"
